a0a6a3bd4349e061c21e13828adfa9f8f99720a5
ale8k <alexjkilroy@gmail.com>
2026-07-14 17:15:49 +0100

    feat: port forward ssh session worker

--- core/ssh/types.go
+++ core/ssh/types.go
@@ -3,6 +3,8 @@
 
 package ssh
 
+import gossh "golang.org/x/crypto/ssh"
+
 // PublicKey represents a single public ssh key for a user within a model.
 type PublicKey struct {
 	// Fingerprint is the calculated fingerprint of the ssh key.
@@ -10,4 +12,15 @@ type PublicKey struct {
 
 	// Key is the raw public key.
 	Key string
+}
+
+// EphemeralKeysUpdater adds and removes ephemeral SSH keys from the machine's
+// authorized_keys file. It is consumed by the sshsession worker, which injects
+// an ephemeral key for the lifetime of a reverse SSH tunnel.
+type EphemeralKeysUpdater interface {
+	// AddEphemeralKey adds an ephemeral key to the authorized_keys file,
+	// tagged with the supplied comment for later removal.
+	AddEphemeralKey(key gossh.PublicKey, comment string) error
+	// RemoveEphemeralKey removes a previously added ephemeral key.
+	RemoveEphemeralKey(key gossh.PublicKey) error
 }
--- internal/worker/authenticationworker/worker_test.go
+++ internal/worker/authenticationworker/worker_test.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/juju/juju/agent"
 	coremachineauthentication "github.com/juju/juju/core/machineauthentication"
+	coressh "github.com/juju/juju/core/ssh"
 	"github.com/juju/juju/core/watcher/watchertest"
 	coretesting "github.com/juju/juju/internal/testing"
 	"github.com/juju/juju/internal/worker/authenticationworker"
@@ -291,7 +292,7 @@ func (s *workerSuite) TestAddAndRemoveEphemeralKey(c *tc.C) {
 	authWorker := s.startWorker(c, ctrl)
 	defer workertest.CleanKill(c, authWorker)
 
-	updater, ok := authWorker.(authenticationworker.EphemeralKeysUpdater)
+	updater, ok := authWorker.(coressh.EphemeralKeysUpdater)
 	c.Assert(ok, tc.IsTrue)
 
 	pub, _, _, _, err := gossh.ParseAuthorizedKey([]byte(sshtesting.ValidKeyThree.Key))
@@ -318,7 +319,7 @@ func (s *workerSuite) TestEphemeralKeyRemovedOnRestart(c *tc.C) {
 
 	authWorker := s.startWorker(c, ctrl)
 
-	updater, ok := authWorker.(authenticationworker.EphemeralKeysUpdater)
+	updater, ok := authWorker.(coressh.EphemeralKeysUpdater)
 	c.Assert(ok, tc.IsTrue)
 
 	pub, _, _, _, err := gossh.ParseAuthorizedKey([]byte(sshtesting.ValidKeyThree.Key))
@@ -363,7 +364,7 @@ func (s *workerSuite) TestAddEphemeralKeyDuringModelChange(c *tc.C) {
 	defer workertest.CleanKill(c, authWorker)
 	s.waitSSHKeys(c, append(s.existingKeys, s.existingEnvKey))
 
-	updater, ok := authWorker.(authenticationworker.EphemeralKeysUpdater)
+	updater, ok := authWorker.(coressh.EphemeralKeysUpdater)
 	c.Assert(ok, tc.IsTrue)
 
 	key4, _, _, _, err := gossh.ParseAuthorizedKey([]byte(sshtesting.ValidKeyFour.Key))
